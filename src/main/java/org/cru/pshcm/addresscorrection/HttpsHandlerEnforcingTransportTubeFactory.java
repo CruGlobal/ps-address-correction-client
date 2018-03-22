2cb12d7fbb391c77d0c1f0dcda9d8df5c37b5fa2
Matt Drees <matt.drees@cru.org>
2018-03-22 11:27:49 -0600

    add some javadocs

--- src/main/java/org/cru/pshcm/addresscorrection/HttpsHandlerEnforcingTransportTubeFactory.java
+++ src/main/java/org/cru/pshcm/addresscorrection/HttpsHandlerEnforcingTransportTubeFactory.java
@@ -4,8 +4,25 @@ import com.sun.istack.internal.NotNull;
 import com.sun.xml.internal.ws.api.pipe.ClientTubeAssemblerContext;
 import com.sun.xml.internal.ws.api.pipe.TransportTubeFactory;
 import com.sun.xml.internal.ws.api.pipe.Tube;
+import java.net.HttpURLConnection;
+import javax.net.ssl.HttpsURLConnection;
 
 /**
+ * This works around a peoplesoft JRE bug with https handling.
+ * The problem is with psft.pt8.pshttp.https.Handler#openConnection(URL, Proxy);
+ * it returns a {@link HttpURLConnection}, instead of a {@link HttpsURLConnection},
+ * when the url is an https url.
+ *
+ * This factory creates a HttpTransportPipe that uses reflection to replace
+ * the URLConnection, if necessary, for each request's
+ * {@link com.sun.xml.internal.ws.api.EndpointAddress}.
+ *
+ *
+ * This assumes that the JRE is an oracle JRE,
+ * that there is no security manager enforcement,
+ * and that the jax-ws library is the oracle built-in one.
+ *
+ *
  * @author Matt Drees
  */
 public class HttpsHandlerEnforcingTransportTubeFactory extends TransportTubeFactory
